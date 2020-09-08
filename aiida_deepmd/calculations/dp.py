bd1812be22769c91f5b25efd1dbdf10c02d0c67d
robinzyb <robinzhuang@outlook.com>
2020-09-08 20:54:01 +0800

    Dry run deepmd success
    merge dptrain and dpfreeze into one deepmd
    copy the datas to copy list
    change the example file with real test file

--- aiida_deepmd/calculations/dp.py
+++ aiida_deepmd/calculations/dp.py
@@ -15,7 +15,7 @@ from aiida import orm
 from aiida.common import CalcInfo, CodeInfo, InputValidationError
 
 
-class DpTrainCalculation(CalcJob):
+class DpCalculation(CalcJob):
     """
     This is a DpTrainCalculation, used to prepare input for
     deepmd dp training.
@@ -24,20 +24,24 @@ class DpTrainCalculation(CalcJob):
     For information on deepmd, refer to: https://github.com/deepmodeling/deepmd-kit
     """
 
-    # Defaults
+    # Defaults for training
     _DEFAULT_INPUT_FILE = 'aiida.json'
-    _DEFAULT_OUTPUT_FILE = 'aiida.out'
+    _DEFAULT_TRAIN_OUTPUT_FILE = 'aiida.out'
     _DEFAULT_PROJECT_NAME = 'aiida'
     _TRAIN_DATA_SUBFOLDER = './data/'
-    _TRAIN_SET_PRFIX = 'set.'
+    _TRAIN_SET_SUBFOLDER = './data/set.000'
     _DEFAULT_OUTPUT_INFO_FILE = 'lcurve.out'
     _DEFAULT_CHECK_META_FILE = 'model.ckpt.meta'
     _DEFAULT_CHECK_INDEX_FILE = 'model.ckpt.index'
     _DEFAULT_CHECK_META_PREFIX = 'model.ckpt.data'
 
+    # Defaults for freeze
+    _DEFAULT_FREEZE_OUTPUT_FILE = 'model.pb'
+    _DEFAULT_PARENT_CALC_FLDR_NAME = './'
+
     @classmethod
     def define(cls, spec):
-        super(DpTrainCalculation, cls).define(spec)
+        super(DpCalculation, cls).define(spec)
 
         # Input parameters
         spec.input('model', valid_type=orm.Dict, help='parameters of model')
@@ -48,16 +52,18 @@ class DpTrainCalculation(CalcJob):
         # TODO: use folder here not store the data which confilct the provenance
         # a special datatype is need to write the files for training and then uploaded
         # spec.input_namespace('file')
-        spec.input('file.box_raw', valid_type=orm.SinglefileData, required=True, help='raw data')
-        spec.input('file.coord_raw', valid_type=orm.SinglefileData, required=True, help='raw data')
-        # spec.input('file.energy_raw', valid_type=orm.SinglefileData, required=True, help='raw data')
-        # spec.input('file.force_raw', valid_type=orm.SinglefileData, required=True, help='raw data')
+        spec.input('file.type_raw', valid_type=orm.SinglefileData, required=True, help='raw or npy data')
+        spec.input('file.box', valid_type=orm.SinglefileData, required=True, help='raw or npy data')
+        spec.input('file.coord', valid_type=orm.SinglefileData, required=True, help='raw or npy data')
+        spec.input('file.energy', valid_type=orm.SinglefileData, required=True, help='raw or npy data')
+        spec.input('file.force', valid_type=orm.SinglefileData, required=True, help='raw or npy data')
+        spec.input('file.virial', valid_type=orm.SinglefileData, required=False, help='raw or npy data')
         #
         # spec.input_namespace('add_file.type_map_raw', valid_type=orm.SinglefileData, required=False, help='raw data')
         # spec.input_namespace('add_file.type_raw', valid_type=orm.SinglefileData, required=False, help='raw data')
 
         # inputs.metadata.options.resources = {}
-        spec.input('metadata.options.withmpi', valid_type=bool, default=True)
+        spec.input('metadata.options.withmpi', valid_type=bool, default=False)
 
         # Exit codes
         spec.exit_code(100,
@@ -92,51 +98,67 @@ class DpTrainCalculation(CalcJob):
             except ValueError as exc:
                 raise InputValidationError("invalid keys or values in input parameters found")
 
-        local_copy_list = []
         # Create the subfolder that will contain the train data
-        folder.get_subfolder(self._TRAIN_DATA_SUBFOLDER, create=True)
-        for fn in self.inputs.file:
-            fobj = self.inputs.file[fn]
-            src_path = fobj.filename
-            dst_path = os.path.join(self._TRAIN_DATA_SUBFOLDER, fobj.filename)
-            local_copy_list.append((fobj.uuid, src_path, dst_path))
 
-        def create_array_from_files(files):
-            for f in files:
-                content = f.get_content()
-                # function from aiida_ce
-                pass
-            return data_array
+        folder.get_subfolder(self._TRAIN_DATA_SUBFOLDER, create=True)
+        folder.get_subfolder(self._TRAIN_SET_SUBFOLDER, create=True)
+
+        # remember to copy type map
+        local_copy_list = []
+        for name, obj in self.inputs.file.items():
+            # if type.map, copy to the ./data
+            if name == 'type_raw':
+                dst_path = os.path.join(self._TRAIN_DATA_SUBFOLDER, obj.filename)
+                local_copy_list.append((obj.uuid, obj.filename, dst_path))
+            # copy other files to the ./data/set.000
+            else:
+                dst_path = os.path.join(self._TRAIN_SET_SUBFOLDER, obj.filename)
+                local_copy_list.append((obj.uuid, obj.filename, dst_path))
+#refactor
+#        def create_array_from_files(files):
+#            for f in files:
+#                data_array = np.loadtxt(f)
+#                # function from aiida_ce
+#                pass
+#            return data_array
 
         # create train set and store the data in
-        data_array = create_array_from_files(self.inputs.file)
+#        box_data = np.loadtxt(self.inputs.file.box_raw)
+#        coord_data = np.loadtxt(self.inputs.file.coord_raw)
+
 
         # for simplicity do not split the folder
-        n = 0
-        set_folder = folder.get_subfolder(os.path.join(self._TRAIN_DATA_SUBFOLDER, self._TRAIN_SET_PRFIX + str(n)), create=True)
-        with io.open(set_folder.get_abs_path(coord.npy), mode="w") as fobj:
-            try:
-                np.dump(fobj, array=data_array)
-            except ValueError as exc:
-                raise InputValidationError("invalid keys or values in input parameters found")
+#        set_folder = folder.get_subfolder(os.path.join(self._TRAIN_DATA_SUBFOLDER, self._TRAIN_SET_PRFIX + str(n)), create=True)
+#        try:
+#            coord_data.dump("coord.npy")
+#        except ValueError as exc:
+ #           raise InputValidationError("invalid keys or values in input parameters found")
 
         # settings = self.inputs.settings.get_dict() if 'settings' in self.inputs else {}
 
-        # create code info
-        codeinfo = CodeInfo()
-        codeinfo.cmdline_params = ["train", self._DEFAULT_INPUT_FILE]
-        codeinfo.stdout_name = self._DEFAULT_OUTPUT_FILE
-        codeinfo.join_files = True
-        codeinfo.code_uuid = self.inputs.code.uuid
+        # set two code info here, once the training finished, the model will freeze then.
+        # create code info for training
+        codeinfotrain = CodeInfo()
+        codeinfotrain.cmdline_params = ["train", self._DEFAULT_INPUT_FILE]
+        codeinfotrain.stdin_name = self._DEFAULT_INPUT_FILE
+        codeinfotrain.stdout_name = self._DEFAULT_TRAIN_OUTPUT_FILE
+        codeinfotrain.join_files = True
+        codeinfotrain.code_uuid = self.inputs.code.uuid
+        codeinfotrain.withmpi = self.inputs.metadata.options.withmpi
+
+        # create code info for freeze
+        codeinfofreeze = CodeInfo()
+        codeinfofreeze.cmdline_params = ["freeze", '-o', self._DEFAULT_FREEZE_OUTPUT_FILE]
+        codeinfofreeze.stdout_name = self._DEFAULT_FREEZE_OUTPUT_FILE
+        codeinfofreeze.code_uuid = self.inputs.code.uuid
+        codeinfofreeze.withmpi = self.inputs.metadata.options.withmpi
+
 
         # create calc info
         calcinfo = CalcInfo()
         calcinfo.uuid = self.uuid
-        calcinfo.cmdline_params = codeinfo.cmdline_params
-        calcinfo.stdin_name = self._DEFAULT_INPUT_FILE
-        calcinfo.stdout_name = self._DEFAULT_OUTPUT_FILE
         calcinfo.local_copy_list = local_copy_list
-        calcinfo.codes_info = [codeinfo]
+        calcinfo.codes_info = [codeinfotrain, codeinfofreeze]
 
         calcinfo.retrieve_list = [
             # self._DEFAULT_OUTPUT_FILE,
@@ -147,75 +169,4 @@ class DpTrainCalculation(CalcJob):
 
         return calcinfo
 
-class DpFreezeCalculation(CalcJob):
-    """
-    This is a DpFreezeCalculation, used to freeze the model.
-    It is used in the BaseRestartWorkChain with train
-    Users are supposed to get a frozen_model file which can be used for seq calculation
-    For information on deepmd, refer to: https://github.com/deepmodeling/deepmd-kit
-    """
 
-    # Defaults
-    _DEFAULT_OUTPUT_FILE = 'model.pb'
-    _DEFAULT_PARENT_CALC_FLDR_NAME = './'
-
-    @classmethod
-    def define(cls, spec):
-        super(DpFreezeCalculation, cls).define(spec)
-
-        # Input parameters
-        spec.input('parent_calc_folder', valid_type=orm.RemoteData, required=True, help='remote folder used for processing')
-
-        # Exit codes
-        spec.exit_code(100,
-                       'ERROR_NO_RETRIEVED_FOLDER',
-                       message='The retrieved folder data node could not be accessed.')
-
-        # Output parameters
-        # parser will parse the retrieved file into froze_model
-        spec.output('model', valid_type=orm.SinglefileData, required=True, help='freeze model')
-
-    def prepare_for_submission(self, folder):
-        """Create the input files from the input nodes passed to this instance of the `CalcJob`.
-
-        :param folder: an `aiida.common.folders.Folder` to temporarily write files on disk
-        :return: `aiida.common.datastructures.CalcInfo` instance
-        """
-
-        # symlinks
-        calcinfo.remote_symlink_list = []
-        calcinfo.remote_copy_list = []
-
-        comp_uuid = self.inputs.parent_calc_folder.computer.uuid
-        remote_path = self.inputs.parent_calc_folder.get_remote_path()
-        copy_info = (comp_uuid, remote_path, self._DEFAULT_PARENT_CALC_FLDR_NAME)
-        if self.inputs.code.computer.uuid == comp_uuid:  # if running on the same computer - make a symlink
-            # if not - copy the folder
-            calcinfo.remote_symlink_list.append(copy_info)
-        else:
-            calcinfo.remote_copy_list.append(copy_info)
-
-
-        # create code info
-        codeinfo = CodeInfo()
-        codeinfo.cmdline_params = ["freeze", '-o', self._DEFAULT_OUTPUT_FILE]
-        codeinfo.stdout_name = self._DEFAULT_OUTPUT_FILE
-        codeinfo.code_uuid = self.inputs.code.uuid
-
-        calcinfo.uuid = self.uuid
-        calcinfo.cmdline_params = codeinfo.cmdline_params
-        calcinfo.codes_info = [codeinfo]
-        calcinfo.remote_copy_list = remote_copy_list
-        calcinfo.remote_symlink_list = remote_symlink_list
-
-        calcinfo.retrieve_list = [
-            self._DEFAULT_OUTPUT_FILE,
-        ]
-
-        # check for left over settings
-        if settings:
-            raise InputValidationError("The following keys have been found " +
-                                       "in the settings input node {}, ".format(self.pk) + "but were not understood: " +
-                                       ",".join(settings.keys()))
-
-        return calcinfo
--- examples/example_01.py
+++ examples/example_01.py
@@ -7,8 +7,9 @@ import os
 from aiida_deepmd import tests, helpers
 from aiida import cmdline, engine
 from aiida.plugins import DataFactory, CalculationFactory
+from aiida.orm import Dict, SinglefileData
 import click
-
+import json
 
 def test_run(deepmd_code):
     """Run a calculation on the localhost computer.
@@ -21,33 +22,55 @@ def test_run(deepmd_code):
         deepmd_code = helpers.get_code(entry_point='deepmd', computer=computer)
 
     # Prepare input parameters
-    DiffParameters = DataFactory('deepmd')
-    parameters = DiffParameters({'ignore-case': True})
-
-    SinglefileData = DataFactory('singlefile')
-    file1 = SinglefileData(
-        file=os.path.join(tests.TEST_DIR, "input_files", 'file1.txt'))
-    file2 = SinglefileData(
-        file=os.path.join(tests.TEST_DIR, "input_files", 'file2.txt'))
+#    DiffParameters = DataFactory('deepmd')
+#    parameters = DiffParameters({'ignore-case': True})
+    with open(os.path.join(tests.TEST_DIR, "input_files", "water_se_a.json")) as f:
+        train_param = json.load(f)
+    box = SinglefileData(
+        file=os.path.join(tests.TEST_DIR, "input_files", 'box.npy'))
+    coord = SinglefileData(
+        file=os.path.join(tests.TEST_DIR, "input_files", 'coord.npy'))
+    energy = SinglefileData(
+        file=os.path.join(tests.TEST_DIR, "input_files", 'energy.npy'))
+    force = SinglefileData(
+        file=os.path.join(tests.TEST_DIR, "input_files", 'force.npy'))
+    typeraw = SinglefileData(
+        file=os.path.join(tests.TEST_DIR, "input_files", 'type.raw'))
 
     # set up calculation
     inputs = {
         'code': deepmd_code,
-        'parameters': parameters,
-        'file1': file1,
-        'file2': file2,
+#        'parameters': parameters,
+        'model': Dict(dict=train_param["model"]),
+        'learning_rate': Dict(dict=train_param["learning_rate"]),
+        'loss': Dict(dict=train_param["loss"]),
+        'training': Dict(dict=train_param["training"]),
+        'file': {
+            'box': box,
+            'coord': coord,
+            'energy': energy,
+            'force': force,
+            'type_raw': typeraw
+        },
         'metadata': {
             'description': "Test job submission with the aiida_deepmd plugin",
+            'dry_run': True,
+            'options':{
+                'resources': {
+                    'tot_num_mpiprocs': 4
+                },
+                'queue_name': 'large'
+            }
         },
     }
 
     # Note: in order to submit your calculation to the aiida daemon, do:
     # from aiida.engine import submit
     # future = submit(CalculationFactory('deepmd'), **inputs)
-    result = engine.run(CalculationFactory('dptrain'), **inputs)
-
-    computed_diff = result['deepmd'].get_content()
-    print("Computed diff between files: \n{}".format(computed_diff))
+    result = engine.run(CalculationFactory('deepmd'), **inputs)
+    print(result)
+#    computed_diff = result['deepmd'].get_content()
+#    print("Computed diff between files: \n{}".format(computed_diff))
 
 
 @click.command()
